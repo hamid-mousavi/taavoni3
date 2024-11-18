c8b84aece420041b03b0e19bb9d5f0955314e1a3
iR <hamidabadani63@gmail.com>
2024-11-18 12:33:13 +0330

    rtl and font add

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,7 +16,7 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        return Redirect("/user");
     }
 [Authorize(Roles = "Admin")]
     public IActionResult Privacy()
